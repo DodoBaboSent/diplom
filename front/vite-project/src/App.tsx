f1a2779bce3f3b54d745ae25a158a20014651150
Артём Наугольнов <lolod.naugolnov@yandex.ru>
2024-04-03 11:37:36 +1000

    Vite+React+Go embed

--- front/vite-project/src/App.tsx
+++ front/vite-project/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>  
-    </>
-  )
+    return (
+        <>
+            <div className="grid-rows-3 grid-cols-[[start-content]_2fr_[end-content_start-nav]_1fr_[end-nav]] grid">
+                <div className="col-start-[start-content] col-end-[end-nav] h-[100px] bg-amber-900"></div>
+                <div className="align-center justify-center flex col-start-1 col-end-2">
+                    <h1 className="text-3xl font-bold underline">
+                        Hello world!
+                    </h1>
+                </div>
+                <div className="align-center justify-center flex col-start-2 col-end-2">
+                    <h1 className="text-2xl font-bold">Nav</h1>
+                </div>
+                <div className="bg-amber-500 col-start-[start-content] col-end-[end-nav]"></div>
+            </div>
+        </>
+    )
 }
 
 export default App
