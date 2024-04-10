5e4527b0b0162cc5c95e13bf1700739288008c46
Артём Наугольнов <bujt@DESKTOP-0JERM76>
2024-04-10 23:42:58 +1000

    remove unnecesary dependencies

--- front/vite-project/src/main.tsx
+++ front/vite-project/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "leaflet/dist/leaflet.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
